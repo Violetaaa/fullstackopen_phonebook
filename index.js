d20b6ad372dab87aa789ca9a95bf341f01802da5
Violeta <valvarezareces@gmail.com>
2020-08-29 20:27:45 +0200

    3.2, 3.3 and 3.4

--- index.js
+++ index.js
@@ -23,6 +23,36 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/info', (req, res) => {
+  const data = `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `
+  res.send(data)
+  })
+
+//display a single phonebook entry
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if(person){
+    res.json(person) 
+  } else {
+    res.status(404).end()
+  }
+})
+
+//delete a single phonebook entry 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
