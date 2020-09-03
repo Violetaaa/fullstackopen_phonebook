efc7dae1b6f80ec84bdfe6bafb487329cb633d16
Violeta <valvarezareces@gmail.com>
2020-09-04 00:43:22 +0200

    heroku

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 // app.use(morgan(tiny));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body-content'))
