fe2692e675ec21335083261d991d5fdea8ba4380
Violeta <valvarezareces@gmail.com>
2020-09-21 12:25:06 +0200

    3.18

--- index.js
+++ index.js
@@ -31,11 +31,12 @@ app.get('/api/persons', (req, res) => {
 
 //display info
 app.get('/api/info', (req, res) => {
-  const data = `
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-  `
-  res.send(data)
+  Person
+    .find()
+    .then(persons => {
+      res.send(`Phonebook has info for ${persons.length} people. </br> ${Date()}`)
+  })
+  .catch(error => next(error))
 })
 
 //display a single phonebook entry
@@ -43,11 +44,11 @@ app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then(person => {
       if (person) {
-        res.json(person.toJSON())
+        res.json(person)
       } else {
         res.status(404).end()
       }
-  })
+    })
     .catch(error => next(error))
 })
 
@@ -60,20 +61,20 @@ app.put('/api/persons/:id', (req, res, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(req.params.id, person, {new : true})
-  .then(updatedPerson=> {
-    res.json(updatedPerson.toJSON())
-  })
-  .catch(error => next(error))
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
 })
 
 //delete a single phonebook entry 
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-  .then(result => {
-    res.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 //add a new phonebook entrie
@@ -97,12 +98,12 @@ app.post('/api/persons', (req, res, next) => {
     number: body.number
   })
 
-   person
+  person
     .save()
     .then(person => {
       // console.log(`added ${person.name} number ${person.number} to phonebook`)
       res.json(person.toJSON())
-  })
+    })
 })
 
 const unknownEndpoint = (req, res) => {
@@ -117,7 +118,7 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === 'CastError' && error.kind == 'ObjectId') {
     return res.status(400).send({ error: 'malformatted id' })
-  } 
+  }
 
   next(error)
 }
@@ -125,7 +126,7 @@ const errorHandler = (error, req, res, next) => {
 app.use(errorHandler)
 
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
